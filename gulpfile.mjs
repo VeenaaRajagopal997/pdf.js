c0dcec568731045dcb60815ea3a69340fbac59a9
VeenaaRajagopal997 <veenaa.rajagopal@waivgen.com>
2024-12-17 20:50:42 +0530

    commit

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1083,7 +1083,7 @@ function buildGeneric(defines, dir) {
       .pipe(gulp.dest(dir + "web")),
 
     gulp
-      .src("web/compressed.tracemonkey-pldi-09.pdf", { encoding: false })
+      .src("web/new_book.pdf", { encoding: false })
       .pipe(gulp.dest(dir + "web")),
   ]);
 }
